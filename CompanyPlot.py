0286be5928f90f0306dbe67a489e1e0cc67e4e28
Andrew van der Zel <avander7@uncc.edu>
2021-04-16 15:22:27 -0400

    Ordering Works now

--- CompanyPlot.py
+++ CompanyPlot.py
@@ -3,10 +3,36 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import numpy as np
 df = pd.read_csv('companyGraph.csv')
-# Sort by row???
-# df.iloc[:, np.argsort(df[28])]
-df.sort_values(by="2015", ascending=0)
 
+
+top_five = []
+
+for i in range(5):
+    high_co2 = 0
+    high_column = None
+    index = 0
+    for c in range(len(df.columns)):
+        if(c == 0):
+            continue
+        
+        column = df.iloc[:,c:c+1]
+        latest_value = float(column.iloc[-1])
+        
+        if(latest_value > high_co2 and list(column) not in top_five):
+            index = c + 1
+            high_co2 = latest_value
+            high_column = column
+    
+    top_five.append(list(high_column))
+    
+
+print(top_five)
+
+
+
+
+
+# Ploting
 plots = []
 for c in df.iloc[:, 1:]:
     fig = go.Scatter(x=df["Year"], y=df[c], name=c)
