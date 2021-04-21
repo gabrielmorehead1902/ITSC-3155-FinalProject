122a6024f647694410aaf2a5ca42bc652e65b22c
Andrew van der Zel <avander7@uncc.edu>
2021-04-20 21:34:49 -0400

    Interactive Buttons

---
+++ Dashboard/toggle.js
@@ -0,0 +1,43 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+function myFunction() {
+var x = document.getElementById("myDIV");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function first() {
+    var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV2");
+
+    x.style.display = "block"
+    y.style.display = "none"
+}
+
+function second() {
+    var x = document.getElementById("myDIV1");
+    var y = document.getElementById("myDIV2");
+
+    x.style.display = "none"
+    y.style.display = "block"
+}
